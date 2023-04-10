2d72fccdda1c99256b2035bb43702f1758aeec2e
DNYDYS <1786087581@qq.com>
2023-04-10 21:07:10 +0800

    tabBar功能开发

---
+++ pages/cate/cate.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    cate: 目录
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
